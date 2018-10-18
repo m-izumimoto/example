e67232f4a95ff42fc9cd55fcebdd6dcfb9a3a34c
izumimoto <izumimoto@interpark.co.jp>
2018-10-18 14:09:21 +0900

    test-API-images

--- index.php
+++ index.php
@@ -22,7 +22,11 @@ $events = $bot->parseEventRequest(file_get_contents('php://input'),$signature);
 // 配列に格納された各イベントをループで処理
 foreach ($events as $event){
   // テキストを返信
-  $bot->replyText($event->getReplyToken(),'TextMessage');
+  // $bot->replyText($event->getReplyToken(),'TextMessage');
+  // テキストを返信し次のイベントの処理へ
+  // replyTextMessage($bot,$event->getReplyToken(),'TextMessage');
+  // 画像を返信する
+  replyImageMessage($bot, $event->getReplyToken(),'https://' . $_SERVER[HTTP_HOST] . '/imgs/original.jpg' , 'https://' . $_SERVER[HTTP_HOST] . '/imgs/preview.jpg');
 }
 
 //テキストを返信。引数はLINEBot、返信先、テキスト
@@ -34,6 +38,15 @@ function replyTextMessage($bot,$replyToken,$text) {
   //レスポンスが異常な場合
   if(!$response->isSucceeded()){
     //エラー内容を出力
+    error_log('Failed! '. $response->getHTTPStatus . ' '.$response->getRawBody());
+  }
+}
+
+//画像を返信。引数はLINEBot、返信先、画像URL、サムネイルURL
+function replyImageMessage($bot,$replyToken,$originalImageUrl,$previewImageUrl){
+  // ImageMessageBuilderの引数は画像URL、サムネイルURL
+  $response = $bot->replyMessage($replyToken, new \LINE\LINEBot\MessageBuilder\ImageMessageBuilder($originalImageUrl, $previewImageUrl));
+  if(!$response->isSucceeded()){
     error_log('Failed! '. $response->getHTTPStatus . ' '.$response->getRawBody());
   }
 }
